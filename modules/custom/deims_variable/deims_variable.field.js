6c280a323626c77f334da537e1f8d638a6047f9f
Dave Reid <dave@davereid.net>
2013-07-02 12:29:50 -0500

    Adding improved UI for variable editing.

---
+++ modules/custom/deims_variable/deims_variable.field.js
@@ -0,0 +1,28 @@
+(function ($) {
+
+/**
+ * Provide the summary information for the block settings vertical tabs.
+ */
+Drupal.behaviors.variableFieldSummary = {
+  attach: function (context) {
+
+    $('details.variable-entry', context).drupalSetSummary(function (context) {
+      var summary = [];
+      var label = $('input.variable-label', context).val() || Drupal.t('Unlabeled');
+      var type = $('select.variable-type option:selected', context).text();
+      summary.push(label);
+      summary.push(type);
+
+      return Drupal.checkPlain(summary.join(' - '));
+    });
+
+    $('details.variable-entry').bind('summaryUpdated', function() {
+      $(this).find('summary').text($(this).drupalGetSummary());
+    })
+    .trigger('summaryUpdated');
+
+    $('details:not([open])').has('.error').attr("open", "open");
+  }
+};
+
+})(jQuery);
