32855ac7f932221d1d61a93420575b64eff7a7fb
Dave Reid <dave@davereid.net>
2013-07-09 11:23:26 -0500

    Added inline documentation to deims_variable.field.js.

--- modules/custom/deims_variable/deims_variable.field.js
+++ modules/custom/deims_variable/deims_variable.field.js
@@ -1,11 +1,12 @@
 (function ($) {
 
 /**
- * Provide the summary information for the block settings vertical tabs.
+ * Provide the summary information for the variable entry field widget.
  */
 Drupal.behaviors.variableFieldSummary = {
   attach: function (context) {
 
+    // Construct the text for the variable entry form.
     $('details.variable-entry', context).drupalSetSummary(function (context) {
       var summary = [];
       var label = $('input.variable-label', context).val() || Drupal.t('Unlabeled');
@@ -16,11 +17,14 @@ Drupal.behaviors.variableFieldSummary = {
       return Drupal.checkPlain(summary.join(' - '));
     });
 
+    // Update the <summary> with the summary text whenever something is changed.
     $('details.variable-entry').bind('summaryUpdated', function() {
       $(this).children('summary').text($(this).drupalGetSummary());
     })
     .trigger('summaryUpdated');
 
+    // If a 'closed' <details> element contains an error inside of it, force it
+    // to display open.
     $('details:not([open])').has('.error').attr("open", "open");
   }
 };
