b4ec57d181ba678580e5ddf0e8105d8c93cee7bc
Inigo San Gil <isangil@lternet.edu>
2013-10-06 23:22:02 -0600

    EML Harvest List modules and features initial commit

---
+++ modules/custom/eml_harvest_list/templates/eml_hl.tpl.php
@@ -0,0 +1,10 @@
+ <?php
+ /**
+ * @file
+ * Default template to use when an entity is rendered using the 'eml_hl' view mode.
+ *
+ * This file can be overridden by creating a new template file in your theme
+ * named eml-hl--ENTITY-TYPE--SUB-TYPE.tpl.php.
+ */
+?>
+<?php print render($content); ?>
