379804faf7315a3516cd6de7c29c3492c54f9881
Inigo San Gil <isangil@lternet.edu>
2016-04-11 14:40:06 -0600

    Updates EML taxa-template per #115
    adds the <taxonomicCoverage> wrapper

--- modules/custom/eml/templates/eml--taxa--taxa.tpl.php
+++ modules/custom/eml/templates/eml--taxa--taxa.tpl.php
@@ -1,5 +1,7 @@
+<taxonomicCoverage>
 <taxonomicClassification>
   <taxonRankName><?php print render($content['field_taxa_rankname']); ?></taxonRankName>
   <taxonRankValue><?php print render($content['field_taxa_scientific']); ?></taxonRankValue>
   <commonName><?php print render($content['field_taxa_common']); ?></commonName>
 </taxonomicClassification>
+</taxonomicCoverage>
