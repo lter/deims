9981f901fe67fca66da670fc79ce6ef25d931fb2
Inigo San Gil <isangil@lternet.edu>
2017-01-23 17:33:37 -0700

    Tested enhancements to PASTA service clients

--- modules/custom/pasta/lib/PastaApi.php
+++ modules/custom/pasta/lib/PastaApi.php
@@ -37,7 +37,12 @@ class PastaApi {
     // ID which is used to fetch the actual evalution report from
     // /error/eml/{transaction}.
     if ($request->code == 202 && !empty($request->data)) {
+      dpm($request->data);
+ 
       return $request->data;
+    }
+    elseif ($request->code == 401) {
+      throw new Exception(t('Unauthorized to submit EML to @url: @error.', array('@url' => $url, '@error' => $request->error)));
     }
     elseif (!empty($request->error)) {
       throw new Exception(t('Unable to submit EML to @url: @error.', array('@url' => $url, '@error' => $request->error)));
@@ -49,12 +54,33 @@ class PastaApi {
 
   public function deleteEml() {
     list($scope, $identifier) = $this->dataSet->getPackageIDParts();
-    $options = array();
+    $url = static::getApiUrl("package/eml/$scope/$identifier");
+    $options = array(
+      'method' => 'DELETE',
+      'headers' => array(
+        'Content-Type' => 'application/xml',
+      ),
+    );
     static::addApiAuthentication($options);
-    $url = static::getApiUrl("eml/$scope/$identifier");
-    $request = drupal_http_request($url, array('method' => 'DELETE'));
+    $request = drupal_http_request($url, $options);
     // @todo Add some kind of exception handling here if the request did not succeed.
-  }
+    if ($request->code == 200) {
+    //  dataset nuked
+      return $request->code;
+    }
+    elseif ($request->code == 401 && !empty($request->error)) {
+      throw new Exception(t('Unauthorized to nuke this dataset from PASTA to @url: @error.', array('@url' => $url, '@error' => $request->error)));
+    }
+    elseif ($request->code == 404 && !empty($request->error)) {
+      throw new Exception(t('Dataset not in PASTA - could not remove it @url: @error.', array('@url' => $url, '@error' => $request->error)));
+    }
+    elseif ($request->code == 400 && !empty($request->error)) {
+      throw new Exception(t('PASTA says illegal identifier or revision @url.', array('@url' => $url, '@error' => $request->error)));
+    }
+    else {
+      throw new Exception(t('Unable to perform remove from PASTA action EML to @url.', array('@url' => $url, '@error' => $request->error)));
+    }
+}
 
   /**
    * Fetch a data set's DOI (data object ID) from the LTER Data Manager API.
@@ -72,11 +98,20 @@ class PastaApi {
     $request = drupal_http_request($url, array('timeout' => 10));
 
     if (empty($request->error) && $request->code == 200 && !empty($request->data)) {
-      if (preg_match('/doi:([\d.]+\/pasta\/[a-z0-9]+)/', $request->data, $matches)) {
+      if (preg_match('/pasta-s/', $url, $matches)) {
+       if (preg_match('/doi:([\d.]+\/FK2\/[a-z0-9]+)/', $request->data, $matches)) {
         return $matches[1];
-      }
-      else {
+       }
+       else {
         throw new Exception(t('DOI request to @url returned expected result %doi.', array('@url' => $url, '%doi' => $request->data)));
+       }
+      }else{
+       if (preg_match('/doi:([\d.]+\/pasta\/[a-z0-9]+)/', $request->data, $matches)) {
+        return $matches[1];
+       }
+       else {
+        throw new Exception(t('DOI request to @url returned expected result %doi.', array('@url' => $url, '%doi' => $request->data)));
+       }
       }
     }
     elseif (!empty($request->error)) {
@@ -86,6 +121,33 @@ class PastaApi {
       throw new Exception(t('Unable to fetch EML DOI from @url.'));
     }
   }
+ /**
+   * Fetch a data set's Eml revision ID (data object version) from the LTER PASTA Data Manager API.
+   *
+   * @return int
+   *   An integer from the PASTA API.
+   *
+   * @throws Exception
+   * @ingroup eml_data_manager_api
+   */
+  public function fetchEMLID() {
+    list($scope, $identifier, $revision) = $this->dataSet->getPackageIDParts();
+
+    $url = static::getApiUrl("package/eml/{$scope}/{$identifier}?filter=newest");
+    $request = drupal_http_request($url, array('timeout' => 10));
+
+    if (empty($request->error) && $request->code == 200 && !empty($request->data)) {
+        return $request->data;
+    }
+    elseif (!empty($request->error)) {
+      throw new Exception(t('Unable to fetch EML ID from @url: @error.', array('@url' => $url, '@er
+ror' => $request->error)));
+    }
+    else {
+      throw new Exception(t('Unable to fetch EML Revision ID from @url.'));
+    }
+  }
+
 
   /**
    * Fetch an EML validation report transaction from the LTER Data Manager API.
@@ -157,6 +219,49 @@ class PastaApi {
     // Do not return anything if we could not fetch validity. We need to be able
     // to distinguish between valid (TRUE), invalid (FALSE), and unknown (NULL).
   }
+
+  /**
+   * Fetch a Report for an dataset EML in PASTA from the LTER Data Manager API.
+   *
+   * @throws Exception
+   * @see PastaApi::fetchEmlReport()
+   */
+  public function fetchEmlReport() {
+
+    list($scope, $identifier, $revision) = $this->dataSet->getPackageIDParts();
+
+    // Fetch the EML report from the API.
+    $url = static::getApiUrl("package/report/eml/{$scope}/{$identifier}/{$revision}");
+    $request = drupal_http_request($url, array('timeout' => 10));
+
+    // The report API on success returns a 200 response with the report XML
+    // in the response body.
+    if ($request->code == 200 && !empty($request->data)) {
+      return static::parseEvaluationSummary($request->data);
+    }
+    elseif ($request->code == 400) {
+      throw new Exception(t('Unable to fetch EML validation report from @url: @error.', array('@url' => $url, '@error' => $response->error)));
+      return array();
+    }
+    elseif ($request->code == 401) {
+      throw new Exception(t('Unauthorized to fetch EML validation report from @url: @error.', array('@url' => $url, '@error' => $response->error)));
+      return array();
+    }
+    elseif ($request->code == 404) {
+      throw new Exception(t('Unable to find EML validation report from @url: @error.', array('@url' => $url, '@error' => $response->error)));
+      return array();
+    }
+    elseif ($request->code == 500) {
+      throw new Exception(t('Server error in fetching EML validation report from @url: @error.', array('@url' => $url, '@error' => $response->error)));
+      return array();
+    }
+    else {
+      throw new Exception(t('Error fetching EML validation report from @url: @error.', array('@url' => $url, '@error' => $response->error)));
+      return array();
+    }
+
+  }
+
 
   /**
    * Parse an EML evalution report into a summary.
